5be24b4bb2d6fe8fc550a6384346d72381427512
Nick <nick_lee@adata.com>
2022-07-01 15:00:22 +0800

    fix:set email password as env variable

--- happy_birthday/settings.py
+++ happy_birthday/settings.py
@@ -176,4 +176,4 @@ EMAIL_HOST = 'smtp.gmail.com'  #SMTP伺服器
 EMAIL_PORT = 587  #TLS通訊埠號
 EMAIL_USE_TLS = True  #開啟TLS(傳輸層安全性)
 EMAIL_HOST_USER = 'chivestsai@gmail.com'  #寄件者電子郵件
-EMAIL_HOST_PASSWORD = 'iwlmwjhqrbcqtnwr'  #Gmail應用程式的密碼
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  #Gmail應用程式的密碼
